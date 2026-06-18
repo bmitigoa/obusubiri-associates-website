e996094d720a93706613dff0d331bb865128e615
bmitigoa <barakamitigoa@gmail.com>
2026-06-18 19:02:17 +0300

    Prepare project for VPS deployment

--- obusubiri/settings.py
+++ obusubiri/settings.py
@@ -10,13 +10,18 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '***REMOVED***'
 
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "obusubiriassociates.co.ke",
+    "www.obusubiriassociates.co.ke",
+]
 
 
 # Application definition
@@ -103,11 +108,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Default primary key field type
