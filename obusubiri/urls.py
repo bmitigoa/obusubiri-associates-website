57d4cb999fedacc52d098d63dd5ee88f087cdc3b
bmitigoa <barakamitigoa@gmail.com>
2026-06-21 17:02:04 +0300

    Add sitemap support

--- obusubiri/urls.py
+++ obusubiri/urls.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib.sitemaps.views import sitemap
+from core.sitemaps import StaticViewSitemap
+
+sitemaps = {
+    "static": StaticViewSitemap,
+}
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("core.urls")),
+
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
 ]
